c8f035548e73bceb8f9501b5b8d775189f2aaa0d
omi-98 <kokitkaronkar@gmail.com>
2024-07-05 11:22:09 +0530

    Onkar:Registration till Mobile Number

--- cypress/e2e/pom-framework/unit-testing/registration_page.cy.js
+++ cypress/e2e/pom-framework/unit-testing/registration_page.cy.js
@@ -1,4 +1,6 @@
 
+//import { it } from "mocha";
+//import { describe } from "mocha";
 import RegistrationPage from "../../../pages/registration_page";
 
 const registerNow = new RegistrationPage();
@@ -11,6 +13,7 @@ describe('Validate the Register Now Page is visible and clickable', () => {
         cy.UncaughtException();
         cy.viewport('macbook-13');
         cy.visit('/');
+        registerNow.gotoRegisterNowPage();
 
     });
 
@@ -19,56 +22,82 @@ describe('Validate the Register Now Page is visible and clickable', () => {
         registerNow.verifyRegisterNowPage();
 
     })
-    it('Validate Positive and Negative Test cases of First Name', () => {
+    describe('Validate Positive and Negative Test cases of First Name', () => {
 
-        registerNow.gotoRegisterNowPage();
 
-        cy.log('Correct name in First name and Submit');
-        registerNow.validateFirstName('Bharath', registerNow.errorFirstName.correctFirstName);
+        it('Correct name in First name and Submit', () => {
+            registerNow.validateFirstName('Bharath', registerNow.errorFirstName.correctFirstName)
+        });
 
-        cy.log('Empty string in First name and Submit');
-        registerNow.validateFirstName('', registerNow.errorFirstName.emptyFirstName);
+        it('Empty string in First name and Submit', () => {
+            registerNow.validateFirstName('', registerNow.errorFirstName.emptyFirstName)
+        });
 
-        cy.log('Numerics in First name and Submit');
-        registerNow.validateFirstName('12', registerNow.errorFirstName.numbersInFirstName);
-
-        cy.log('Special Character in First name and Submit');
-        registerNow.validateFirstName('@!', registerNow.errorFirstName.specialCharFirstName);
-
-        cy.log('Min character length in first name and Submit');
-        registerNow.validateFirstName('B', registerNow.errorFirstName.minLengthFirstName);
-
-        cy.log('Max char length in first name and Submit');
-        registerNow.validateFirstName(maxLengthFN,
-            registerNow.errorFirstName.maxLengthFirstName);
-
+        it('Numerics in First name and Submit', () => {
+            registerNow.validateFirstName('12', registerNow.errorFirstName.numbersInFirstName);
+        });
+        it('Special Character in First name and Submit', () => {
+            registerNow.validateFirstName('@!', registerNow.errorFirstName.specialCharFirstName);
+        });
+        it('Min character length in first name and Submit', () => {
+            registerNow.validateFirstName('B', registerNow.errorFirstName.minLengthFirstName);
+        });
+        it('Max char length in first name and Submit', () => {
+            registerNow.validateFirstName(maxLengthFN,
+                registerNow.errorFirstName.maxLengthFirstName);
+        });
     });
 
-    it('Validate Positive and Negative Test cases of Last Name', () => {
+    describe('Validate Positive and Negative Test cases of Last Name', () => {
+        //registerNow.gotoRegisterNowPage();
 
-        registerNow.gotoRegisterNowPage();
-
-        cy.log('Correct name in last name and Submit');
-        registerNow.validateLastName('Bharath', registerNow.errorLastName.correctLastName);
-
-        cy.log('Empty string in last name and Submit');
-        registerNow.validateLastName('', registerNow.errorLastName.emptyLastName);
-
-        cy.log('Numerics in last name and Submit');
-        registerNow.validateLastName('12', registerNow.errorLastName.numbersInLastName);
-
-        cy.log('Special Character in last name and Submit');
-        registerNow.validateLastName('@!', registerNow.errorLastName.specialCharLastName);
-
-        cy.log('Min character length in last name and Submit');
-        registerNow.validateLastName('B', registerNow.errorLastName.minLengthLastName);
-
-        cy.log('Max char length in first name and Submit');
-        registerNow.validateLastName(maxLengthFN,
-            registerNow.errorLastName.maxLengthLastName);
-
+        it('Correct name in last name and Submit', () => {
+            registerNow.validateLastName('Bharath', registerNow.errorLastName.correctLastName);
+        });
+        it('Empty string in last name and Submit', () => {
+            registerNow.validateLastName('', registerNow.errorLastName.emptyLastName);
+        });
+        it('Numerics in last name and Submit', () => {
+            registerNow.validateLastName('12', registerNow.errorLastName.numbersInLastName);
+        });
+        it('Special Character in last name and Submit', () => {
+            registerNow.validateLastName('@!', registerNow.errorLastName.specialCharLastName);
+        });
+        it('Min character length in last name and Submit', () => {
+            registerNow.validateLastName('B', registerNow.errorLastName.minLengthLastName);
+        });
+        it('Max char length in first name and Submit', () => {
+            registerNow.validateLastName(maxLengthFN,
+                registerNow.errorLastName.maxLengthLastName);
+        });
     });
 
+    describe('Validate Positive and Negative Test cases of Email Field', () => {
+        // registerNow.verifyRegisterNowPage();
 
+        it('invalid email id in emai id field and submit', () => {
+            registerNow.validateEmailIdField('  ', registerNow.errorEmailField.emailErrorMsg)
+        });
+        it('Correct email id in email id field and Submit', () => {
+            registerNow.validateEmailIdField('ok@gmail.com', registerNow.errorEmailField.validEmailId);
+        });
 
+    })
+
+    describe.only('Validate the mobile number field and submit', () => {
+        //  registerNow.verifyRegisterNowPage();
+
+        it('Empty string in mobile number and submit', () => {
+            registerNow.validateMobileNumberField('', registerNow.errorMobileNumberField.emptyMobileNumberField)
+        });
+        it('Enter less than 10 digits number and submit', () => {
+            registerNow.validateMobileNumberField('12345', registerNow.errorMobileNumberField.minLengthMobileNumber)
+        });
+        it('Enter less than 10 digits number and submit', () => {
+            registerNow.validateMobileNumberField('9898989898989', registerNow.errorMobileNumberField.maxLengthMobileNumber)
+        });
+        it('Enter valid mobile number and submit', () => {
+            registerNow.validateMobileNumberField('9898989898', registerNow.errorMobileNumberField.validMobileNumber)
+        })
+    })
 });
