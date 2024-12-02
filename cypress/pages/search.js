aa1a02c4c09ce5d2de36d1d0dfe64bc84083cb62
Bharath Battini <bharath.bathini@atriina.com>
2024-12-02 16:31:49 +0530

    refactor: Changed search cancel button

--- cypress/pages/search.js
+++ cypress/pages/search.js
@@ -33,7 +33,7 @@ class Search {
         VerifySearchName: () => cy.xpath("//div[contains(@class, 'NewHits_card')]"),
         VerificationcodeInput: () => cy.get("#privateCodeAcademy"),
         SubmitVerificationcodeInput: () => cy.get("[class*='btn PrivateVideoVerificationModal_submit-btn__']"),
-        CancelSearchbutton: () => cy.xpath("//img[@class='cursor-pointer NewSearchModal_close__wFadY']")
+        CancelSearchbutton: () => cy.get("img[class*='NewSearchModal_close__']")
 
     }
 
@@ -46,37 +46,37 @@ class Search {
     clickOnEachBestShows() {
         // Ensure the title is visible before proceeding
         this.elements.bestShowTitleText().should('be.visible');
-    
+
         // Alias the best shows grid view and re-fetch children as needed
         this.elements.bestShowsGridView().children().as('bestShows');
-    
+
         // Get the list length within the loop to avoid detachment
         cy.get('@bestShows').then(($shows) => {
             const showLength = $shows.length;
-    
+
             for (let i = 0; i < showLength; i++) {
                 // Re-fetch the list and check visibility of each show item before clicking
                 cy.get('@bestShows').eq(i).should('exist').click({ timeout: 30000 });
-    
+
                 // Wait to allow any UI updates after each click
                 cy.wait(3000);
-    
+
                 // Check for the presence of the verification code box
                 cy.get('body').then(($body) => {
                     if ($body.find("#privateCodeAcademy").length > 0) {
                         // Input the verification code if the box is present
-                        this.elements.VerificationcodeInput().should('be.visible').type('Dydroboon'); 
-    
+                        this.elements.VerificationcodeInput().should('be.visible').type('Dydroboon');
+
                         // Click the submit button
                         this.elements.SubmitVerificationcodeInput().should('be.visible').click();
-    
+
                         // Wait for the process to complete
                         cy.wait(2000);
                     } else {
                         cy.log('Verification code box not found, proceeding...');
                     }
                 });
-    
+
                 // Execute the search button click action
                 this.clickOnSearchButton();
             }
@@ -95,43 +95,43 @@ class Search {
                 cy.log(`Number of elements matched: ${elements.length}`);
                 cy.wrap(elements[1]).click();  // Clicks the first match (search)
             });
-            
-            
+
+
             // Wait for search results and verify that the correct show name is displayed
             this.elements.VerifySearchName().should('contain', show); // 4. Verify the show name is in the search results
             this.elements.CancelSearchbutton().click(); //5. Verify the Cancel button in the search section
         });
     }
-    
-    
-}   
 
-    const readCsvFile = (csvFileName) => {
-        // Call the Cypress task 'readCsvFile' and pass the file name as an argument
-        return cy.task('readCsvFile', csvFileName).then((csvContent) => {
-            // Return a new Promise to process the CSV content
-            return new Promise((resolve, reject) => {
-                // Use the Papa.parse library to parse the CSV content
-                Papa.parse(csvContent, {
-                    header: true, // Specify that the CSV file contains a header row
-                    delimiter: ',', // Explicitly specify the delimiter (comma in this case)
-                    complete: (result) => {
-                        // Callback function executed when parsing is complete
-                        if (result.errors.length) {
-                            // If there are any parsing errors, reject the promise
-                            reject(result.errors);
-                        } else {
-                            // Otherwise, resolve the promise with an array of 'showName' values
-                            resolve(result.data.map((row) => row.showName)); //Extract show names
-                        }
-                    },
-                });
+
+}
+
+const readCsvFile = (csvFileName) => {
+    // Call the Cypress task 'readCsvFile' and pass the file name as an argument
+    return cy.task('readCsvFile', csvFileName).then((csvContent) => {
+        // Return a new Promise to process the CSV content
+        return new Promise((resolve, reject) => {
+            // Use the Papa.parse library to parse the CSV content
+            Papa.parse(csvContent, {
+                header: true, // Specify that the CSV file contains a header row
+                delimiter: ',', // Explicitly specify the delimiter (comma in this case)
+                complete: (result) => {
+                    // Callback function executed when parsing is complete
+                    if (result.errors.length) {
+                        // If there are any parsing errors, reject the promise
+                        reject(result.errors);
+                    } else {
+                        // Otherwise, resolve the promise with an array of 'showName' values
+                        resolve(result.data.map((row) => row.showName)); //Extract show names
+                    }
+                },
             });
         });
-    };
+    });
+};
 
-        
 
 
-    
+
+
 export { Search, readCsvFile };
